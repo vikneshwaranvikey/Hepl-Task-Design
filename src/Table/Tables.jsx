0cd5b86baee08abbcfee4d8d05f70917c6801b1a
vikneshwaranvikey <vikeyviknesh@gmail.com>
2023-12-18 14:16:24 +0530

    changes

--- src/Table/Tables.jsx
+++ src/Table/Tables.jsx
@@ -1,12 +1,13 @@
 
-import { Space, Table, message, Modal } from 'antd';
+import { Space, Table, message, Modal, Input } from 'antd';
 import { useEffect, useState } from 'react';
 
 function Tables(props) {
 
     const { searchData } = props  //passData Destructure
-    const [dataSource, setDataSource] = useState(searchData)
-
+    const [dataSource, setDataSource] = useState(searchData);
+    const [editrecord, setEditrRecord] = useState(null);
+    const [isEdit, setIsEdit] = useState(false);
     useEffect(() => {
         // You can use dataSource in useEffect or perform other operations
         setDataSource(searchData);
@@ -52,9 +53,12 @@ function Tables(props) {
             title: 'Action',
             render: (record) => (
                 <Space size="middle">
-                    <a href='icon'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-orange-300 fill-orange-400">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-                    </svg>
+                    <a onClick={(e) => {
+                        e.preventDefault();
+                        onEditRecord(record);
+                    }} href='icon'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-orange-300 fill-orange-400">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                        </svg>
                     </a>
 
                     <a onClick={(e) => {
@@ -64,12 +68,15 @@ function Tables(props) {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                         </svg>
                     </a>
-
                 </Space>
             ),
         }
     ];
 
+    const onEditRecord = (record) => {
+        setIsEdit(true);
+        setEditrRecord({ ...record })
+    }
     const onDeleteStudent = (record) => {
         Modal.confirm({
             title: "Are you sure, you want to delete this record?",
@@ -82,9 +89,40 @@ function Tables(props) {
             },
         });
     };
-
+    const resetEditing = () => {
+        setIsEdit(false);
+        setEditrRecord(null);
+    };
     return (
-        <div ><Table className=' font-bold' columns={columns} style={{ fontSize: "bold" }} dataSource={dataSource} /></div>
+        <div ><Table className=' font-bold' columns={columns} style={{ fontSize: "bold" }} dataSource={dataSource} />
+            <Modal
+                title="Edit Student"
+                visible={isEdit}
+                okText="Save"
+                okType="danger"
+                onCancel={() => {
+                    resetEditing();
+                }}
+                onOk={() => {
+                    setDataSource((pre) => {
+                        return pre.map((student) => {
+                            if (student.id === editrecord.id) {
+                                return editrecord;
+                            } else {
+                                return student;
+                            }
+                        });
+                    });
+                    resetEditing();
+                }}
+            >
+
+                <Input value={editrecord?.name} onChange={(e) => setEditrRecord((pre) => { return { ...pre, name: e.target.value } })} />
+                <Input value={editrecord?.email} onChange={(e) => setEditrRecord((pre) => { return { ...pre, email: e.target.value } })} />
+                <Input value={editrecord?.address.street} onChange={(e) => setEditrRecord((pre) => { return { ...pre, address: e.target.value } })} />
+
+            </Modal>
+        </div>
     )
 }
 
